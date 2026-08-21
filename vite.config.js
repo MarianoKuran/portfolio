110fcec499c38f5bf13fbd2ffb563342d247253d
Mariano Kuran <marianokuran6@gmail.com>
2026-08-20 21:18:27 -0300

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        services: resolve(__dirname, 'services.html'),
+        projects: resolve(__dirname, 'projects.html'),
+      },
+    },
+  },
 })
